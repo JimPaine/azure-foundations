c222ad99263be2bad8078ed735eb540475484c4a
Jim Paine <jim@jimpaine.io>
2021-07-26 16:34:09 +0100

    added policy with workflow

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ Templates and steps to take to manage management groups, subscriptions and polic
 az role assignment create  --scope '/' --role 'Owner' --assignee-object-id $(az ad signed-in-user show --query objectId -o tsv)
 
 ```
+
+```
+az ad sp create-for-rbac --name "policyManager" --sdk-auth
+```
+
+```
+az role assignment create  --scope '/providers/Microsoft.Management/managementGroups/azurefoundation' --role 'Resource Policy Contributor' --assignee-object-id {id}
+```
+
+az ad sp create-for-rbac \
+  --name "policyManager" \
+  --role "Resource Policy Contributor" \
+  --scopes  "/providers/Microsoft.Management/managementGroups/azurefoundation" \
+  --sdk-auth
--- main.sh
+++ main.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
+exit_with_error() {
+  echo "Usage: $0 -t {TENANT} -l {LOCATION} -p {DEPLOY_POLICIES}" 1>&2
+  exit 1
+}
+
+while getopts :t:l:p: option
+do
+  case "${option}"
+    in
+      t) tenant=${OPTARG};;
+      l) location=${OPTARG};;
+      p) deploy_policies=${OPTARG};;
+      *) exit_with_error;;
+  esac
+done
+
+az login --tenant "$tenant"
+
+az role assignment create  \
+  --scope '/' \
+  --role 'Owner' \
+  --assignee-object-id "$(az ad signed-in-user show --query objectId -o tsv)"
+
 az deployment tenant create \
   --name azure-foundations \
-  --location uksouth \
+  --location "$location" \
   --template-file main.bicep
+
+if [ $deploy_policies == "true" ]
+then
+  echo "deploying policies"
+fi
